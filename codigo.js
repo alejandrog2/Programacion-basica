78aec7c925e89a6395311de999220095e388467c
Daniel Alejandro Guerrero Suárez <alejandrog28@outlook.com>
2014-08-17 19:55:48 -0500

    Variables y funciones
    Profundizamos en el tema de variables y funciones :D

--- codigo.js
+++ codigo.js
@@ -1,3 +1,18 @@
 // JavaScript Document
-alert("Hola mama ya sé programar!");
-alert(5*4);
+//var nombre ="Alejandro";
+//var apellido ="Guerrero";
+//var edad =28;
+
+//alert(nombre + " " + apellido + "\n" + edad + " años.");
+//alert(5*8 +"2");
+
+var peso;
+var pesoEnMarte;
+
+alert("¿Quieres saber tu peso en marte?");
+peso = prompt("¿Cuál es tu peso en kg?", "70");
+peso = parseInt(peso);
+
+pesoEnMarte = (peso / 9.81) * 3.711;
+
+alert("Tu peso en marte es: " + pesoEnMarte + " kg");
